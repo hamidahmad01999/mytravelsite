0f15025fe87ccc51844d491fceb64a9f58474176
hamidahmad01999 <hamidahmad01999@gmail.com>
2023-01-19 15:57:37 +0500

    This is almost done

---
+++ travel.js
@@ -0,0 +1,15 @@
+// var destination_text= document.querySelector('.destination-text')
+// var book = document.getElementById('img-booking')
+// book.style.display='none'
+// console.log("It's Hamid")
+// destination_text.onmouseover=function(){
+//     if(book.style.display=='none'){
+//         book.style.display='block'
+//     }
+    
+// }
+// var button = document.getElementById('btn');
+// button.onclick=function(){
+//     alert("Its click");
+//     console.log("Hamid")
+// }
